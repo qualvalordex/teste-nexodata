9170eee5d42c00a8550660fe043cb03a4d2a2e78
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2021-11-24 20:30:33 -0300

    :sparkles: add product controllers, services and modules

---
+++ src/products/products.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from './product.model';
+
+@Injectable()
+export class ProductsService {
+  public products: Product[] = [
+    { productId: 1, isActive: true, currentQuantity: 10 },
+    { productId: 2, isActive: true, currentQuantity: 0 },
+    { productId: 1, isActive: false, currentQuantity: 3 },
+  ];
+}
