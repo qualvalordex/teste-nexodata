42fb75da890127a02d58e4bda7c8e07fef75de2d
Paulo Henrique Rodrigues <qualvalordex@gmail.com>
2021-11-24 19:40:23 -0300

    :sparkles: add client provider, controller and service

---
+++ src/clients/clients.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { Client } from './client.model';
+import { ClientsService } from './clients.service';
+
+@Controller('clients')
+export class ClientsController {
+  constructor(
+    @Inject(ClientsService)
+    private clientsService: ClientsService,
+  ) {}
+
+  @Get()
+  findOne(id: number): Client {
+    return this.clientsService.findOne(id);
+  }
+
+  @Get()
+  findall(): Client[] {
+    return this.clientsService.findAll();
+  }
+}
---
+++ src/clients/clients.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { Client } from './client.model';
+
+@Injectable()
+export class ClientsService {
+  private readonly clients: Client[] = [
+    { clientId: 1, name: 'Paulo' },
+    { clientId: 2, name: 'Beatriz' },
+    { clientId: 3, name: 'Larissa' },
+    { clientId: 4, name: 'Bárbara' },
+  ];
+
+  findAll(): Client[] {
+    return this.clients;
+  }
+
+  findOne(id: number): Client {
+    return this.clients.find((client) => client.clientId === id);
+  }
+}
